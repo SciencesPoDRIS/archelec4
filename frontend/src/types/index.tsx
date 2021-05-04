3ccf39db85f06ab4c1052216be2cdff395fa60e3
Benoit Simard <contact@bsimard.com>
2021-05-04 18:39:13 +0200

    [frontend]: Generate types from backend

---
+++ frontend/src/types/index.tsx
@@ -0,0 +1,4 @@
+import { components } from "./api";
+
+export type ArchiveElectoralCandidat = components["schemas"]["ArchiveElectoralCandidat"];
+export type ArchiveElectoralItem = components["schemas"]["ArchiveElectoralItem"];
