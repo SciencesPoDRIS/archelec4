ee06c026056dbb53c5a338c7aec5e31e4ede7336
Paul Girard <paul@ouestware.com>
2021-05-06 18:25:03 +0200

    frontend: use API get profession

--- frontend/src/pages/profession.tsx
+++ frontend/src/pages/profession.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Loader } from "../components/loader";
 import { MetadataPanel } from "../components/metadata-panel";
-import { config } from "../config";
+
 import { useGet } from "../hooks/api";
-import { PlainObject } from "../types";
+import { ProfessionDeFoi } from "../types";
 
 interface Props {
   id: string;
@@ -11,24 +11,30 @@ interface Props {
 export const Profession: React.FC<Props> = (props: Props) => {
   const { id } = props;
 
-  const { data: profession, loading } = useGet<PlainObject>(`${config.api_path}/elasticsearch/proxy/_doc/${id}`);
+  const { data: professionDeFoi, loading } = useGet<ProfessionDeFoi>(`/professiondefoi/${id}`);
 
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-4">{loading ? <Loader /> : <MetadataPanel />}</div>
+        <div className="col-4">
+          {loading && <Loader />}
+          {professionDeFoi && <MetadataPanel professionDeFoi={professionDeFoi as ProfessionDeFoi} />}
+        </div>
         <div className="col-8">
           {loading && <Loader />}
-          {id && (
-            <iframe
-              title={"profession.title"}
-              src={`https://archive.org/embed/${id}`}
-              width="560"
-              height="384"
-              frameBorder="0"
-              allowFullScreen
-            ></iframe>
-          )}
+          <h1>{professionDeFoi && professionDeFoi.title}</h1>
+          <div>
+            {id && (
+              <iframe
+                title={"profession.title"}
+                src={`https://archive.org/embed/${id}`}
+                width="560"
+                height="384"
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            )}
+          </div>
         </div>
       </div>
     </div>
