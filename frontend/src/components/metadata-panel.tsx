ea48dcb1aef7dcc2742bfa865ac4c79747ae04ce
Paul Girard <paul@ouestware.com>
2021-05-06 18:24:31 +0200

    frontend: metadata panel first draft

--- frontend/src/components/metadata-panel.tsx
+++ frontend/src/components/metadata-panel.tsx
@@ -1,5 +1,41 @@
 import React from "react";
+import { ProfessionDeFoi } from "../types";
 
-export const MetadataPanel: React.FC<{}> = () => {
-  return <div>Metadata</div>;
+interface Props {
+  professionDeFoi: ProfessionDeFoi;
+}
+
+export const MetadataPanel: React.FC<Props> = (props: Props) => {
+  const { professionDeFoi } = props;
+  const titulaire = professionDeFoi.candidats.find((c) => c.type === "titulaire");
+  return (
+    <div>
+      <>
+        <div>
+          <h4>
+            Élection {professionDeFoi["contexte-election"]} de {professionDeFoi.annee}
+          </h4>
+          <div>
+            Le{" "}
+            {new Intl.DateTimeFormat("fr-FR", {
+              weekday: "long",
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            }).format(new Date(professionDeFoi.date))}
+          </div>
+        </div>
+        {titulaire && (
+          <div>
+            <h4>
+              {titulaire.prenom} {titulaire.nom} (titulaire)
+            </h4>
+            <div>
+              {titulaire.age} {titulaire["age-calcule"]} {titulaire["age-tranche"]}
+            </div>
+          </div>
+        )}
+      </>
+    </div>
+  );
 };
