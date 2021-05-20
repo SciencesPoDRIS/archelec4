4200d30596fb0485ea4e854457e45396f0cccb63
Paul Girard <paul@ouestware.com>
2021-05-20 10:43:28 +0200

    [frontend] Metadata panel enhanced - review from archelec team - licences

--- frontend/src/components/metadata-panel.tsx
+++ frontend/src/components/metadata-panel.tsx
@@ -1,6 +1,10 @@
 import { listenerCount } from "node:events";
 import React from "react";
 import { Candidat, ProfessionDeFoi } from "../types";
+import byLogo from "../assets/by.png";
+import ccLogo from "../assets/cc.png";
+import ncLogo from "../assets/nc.png";
+import ndLogo from "../assets/nd.png";
 
 interface Props {
   professionDeFoi: ProfessionDeFoi;
@@ -10,9 +14,9 @@ const MetadataList: React.FC<{ list: string[]; label?: string }> = (props: { lis
   const { list, label } = props;
   return (
     <>
-      {list && (
+      {list && list.length > 0 && (
         <div className="metadata-line">
-          <i>{label && `${label} : `}</i>
+          {label && `${label} : `}
           {list.map((p, i) => (
             <span key={i}>
               <span>{p}</span>
@@ -38,7 +42,7 @@ const CandidatMetadata: React.FC<{ candidat: Candidat }> = (props: { candidat: C
             Âge déclaré : {candidat.age || "aucun"}{" "}
             {candidat["age-calcule"] !== candidat.age && <span>(calculé : {candidat["age-calcule"]})</span>}
           </div>
-          <div>Genre : {candidat.sexe}</div>
+          <div>Sexe : {candidat.sexe}</div>
         </div>
       )}
 
@@ -46,8 +50,8 @@ const CandidatMetadata: React.FC<{ candidat: Candidat }> = (props: { candidat: C
         <MetadataList list={candidat.profession} label="Profession" />
         <MetadataList list={candidat["mandat-en-cours"]} label="Mandat" />
         <MetadataList list={candidat["mandat-passe"]} label="Mandat passé" />
-        <MetadataList list={candidat.associations} label="Associations" />
-        <MetadataList list={candidat["autres-statuts"]} label="Autres" />
+        <MetadataList list={candidat.associations} label="Association" />
+        <MetadataList list={candidat["autres-statuts"]} label="Autre statut" />
         {candidat.decorations && candidat.decorations !== "non" && <div>Décorations : {candidat.decorations}</div>}
       </div>
       <div className="metadata-group">
@@ -68,15 +72,15 @@ export const MetadataPanel: React.FC<Props> = (props: Props) => {
       <>
         <div className="metadata-section">
           <div className="panel-header">
-            Élection {professionDeFoi["contexte-election"]} de {professionDeFoi.annee}
+            Élections {professionDeFoi["contexte-election"]} de {professionDeFoi.annee}
           </div>
           <h4>
             {professionDeFoi.circonscription}
-            <sup>{professionDeFoi.circonscription === "1" ? "ère" : "ème"}</sup> circonscription de{" "}
-            {professionDeFoi["departement-nom"]} ({professionDeFoi.departement})
+            <sup>{professionDeFoi.circonscription === "1" ? "er" : "e"}</sup> circ. {professionDeFoi["departement-nom"]}{" "}
+            ({professionDeFoi.departement})
           </h4>
           <div className="metadata-group">
-            <div>
+            <h5>
               {professionDeFoi["contexte-tour"] === "1" ? "Premier" : "Second"} tour, le{" "}
               {professionDeFoi.date &&
                 new Intl.DateTimeFormat("fr-FR", {
@@ -85,7 +89,7 @@ export const MetadataPanel: React.FC<Props> = (props: Props) => {
                   month: "long",
                   day: "numeric",
                 }).format(new Date(professionDeFoi.date))}
-            </div>
+            </h5>
           </div>
         </div>
         {titulaire && <CandidatMetadata candidat={titulaire} />}
@@ -94,7 +98,7 @@ export const MetadataPanel: React.FC<Props> = (props: Props) => {
           <h3>Accès au document</h3>
           <div className="metadata-group">
             <div>
-              Côte {professionDeFoi.cote} aux{" "}
+              Cote {professionDeFoi.cote} aux{" "}
               <a href="https://www.sciencespo.fr/cevipof/fr/content/archives-electorales.html">
                 Archives électorales du CEVIPOF
               </a>
@@ -102,6 +106,19 @@ export const MetadataPanel: React.FC<Props> = (props: Props) => {
             <div>
               Id {professionDeFoi.id} sur{" "}
               <a href={`https://archive.org/details/${professionDeFoi.id}`}>Internet Archive</a>
+            </div>
+            <div>
+              <a
+                className="licenses"
+                rel="license"
+                title="Attribution-NonCommercial-NoDerivs 4.0 International"
+                href="https://creativecommons.org/licenses/by-nc-nd/4.0/"
+              >
+                Licence <img src={ccLogo} alt="Creative Commons License" />
+                <img src={byLogo} alt="by" />
+                <img src={ncLogo} alt="nc" />
+                <img src={ndLogo} alt="nd" />
+              </a>
             </div>
           </div>
         </div>
