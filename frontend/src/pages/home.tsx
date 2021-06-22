26de380920a8d36b316befa9b72f53601bf3afd2
Benoit Simard <contact@bsimard.com>
2021-06-22 10:29:54 +0200

    [frontend] Wording #12

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -9,22 +9,37 @@ export const Home: React.FC<Props> = (props: Props) => {
         <h1>
           Archives électorales de <span style={{ whiteSpace: "nowrap" }}>Sciences Po</span>
         </h1>
-        <h2>Explorer les professions de foi des élections législatives de la Ve République</h2>
+        <h2>
+          Explorer les professions de foi des élections législatives de la V<sup>e</sup> République
+        </h2>
         <p>
-          Interface de recherche avancée pour une exploration multicritère des archives électorales mis en ligne par
-          Sciences Po sur Internet Archive (+ lien) et sur sa Bibliothèque numérique (+ lien).
+          Interface de recherche avancée pour une exploration multicritère des archives électorals du CEVIPOF mises en
+          ligne à partir du corpus diffusé sur
+          <a
+            title="Lien vers le corpus sur Internet Archive"
+            href="https://archive.org/details/archiveselectoralesducevipof"
+          >
+            Internet Archive
+          </a>
+          et sur la{" "}
+          <a
+            title="Lien vers le corpus sur la bibliothèque numérique de Sciences Po"
+            href="https://bibnum.sciencespo.fr/s/catalogue/item-set/1725987"
+          >
+            bibliothèque numérique de Sciences Po
+          </a>
+          .
         </p>
-        <p>Mentionner COLLEX ?</p>
         <p>
           <Link
             className="btn btn-lg"
             to="/explorer"
             title="Explorer les professions de foi des élections législatives de la Ve République"
           >
-            <i className="fas fa-search"></i> explorer
+            <i className="fas fa-search"></i> Explorer
           </Link>
           <Link className="btn btn-lg" to="/faq" title="Foire aux questions">
-            <i className="far fa-question-circle"></i> faq
+            <i className="far fa-question-circle"></i> FAQ
           </Link>
         </p>
       </div>
