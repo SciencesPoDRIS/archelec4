d2883ae250e2f8edad66b5946fcd9d238840ce7e
Paul Girard <paul@ouestware.com>
2022-01-10 16:53:34 +0100

    [frontend] legend in cartography #32

--- frontend/src/components/viz/Cartography.tsx
+++ frontend/src/components/viz/Cartography.tsx
@@ -3,7 +3,6 @@ import FranceSVGParts from "./france_DOM_COM_hexagonal.json";
 import { CartographyDataType } from "../../types/viz";
 import { keyBy, max } from "lodash";
 import chroma from "chroma-js";
-import { useHistory } from "react-router-dom";
 import { useStateUrl } from "../../hooks/state-url";
 import { SEPARATOR } from "../filters/utils";
 
@@ -12,6 +11,7 @@ export const Cartography: FC<{ data: CartographyDataType; width?: number; height
   width,
   height,
 }) => {
+  // TODO: ADD TOOLTIP
   const [termsUrl, setTermsUrl] = useStateUrl<string>("departement-insee", "");
   const [selectedDepartements, setSelectedDepartements] = useState<string[]>([]);
   useEffect(() => {
@@ -24,30 +24,52 @@ export const Cartography: FC<{ data: CartographyDataType; width?: number; height
   const colorScale = chroma.scale(["white", "red"]);
 
   return (
-    <svg version="1.1" width={width || 1000} height={height || 1000} xmlns="http://www.w3.org/2000/svg">
-      {FranceSVGParts.map((p) => {
-        const depFilterKey = dataByINSEEDep[p.insee_dep]?.["departement-insee"];
-        const selected = selectedDepartements.includes(depFilterKey);
-        return (
-          <path
-            onClick={() => {
-              // TODO: refine the selection or add "code - name" key in cartographic data (json)
-              if (depFilterKey)
-                if (selected) setTermsUrl(selectedDepartements.filter((e) => e !== depFilterKey).join(SEPARATOR));
-                else setTermsUrl(selectedDepartements.concat(depFilterKey).join(SEPARATOR));
-            }}
-            d={p.d}
-            key={p.insee_dep}
-            id={p.insee_dep}
-            stroke="black"
-            fill={
-              termsUrl === "" || selected
-                ? colorScale((dataByINSEEDep[p.insee_dep]?.doc_count || 0) / maxCount).hex()
-                : "lightgrey"
-            }
-          />
-        );
-      })}
-    </svg>
+    <div>
+      <h2>Nombre de professions de foi par département</h2>
+      <div className="legend d-flex align-items-center">
+        Nombre de Profession de foi: 0{" "}
+        <div
+          className="mx-2"
+          style={{
+            display: "inline-block",
+            height: "20px",
+            width: "100px",
+            background: "linear-gradient(to right,white, red)",
+          }}
+        />
+        {maxCount}
+      </div>
+      <svg
+        version="1.1"
+        width={width || 1000}
+        height={height || 1000}
+        viewBox="0 0 1000 1000"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        {FranceSVGParts.map((p) => {
+          const depFilterKey = dataByINSEEDep[p.insee_dep]?.["departement-insee"];
+          const selected = selectedDepartements.includes(depFilterKey);
+          return (
+            <path
+              onClick={() => {
+                // TODO: refine the selection or add "code - name" key in cartographic data (json)
+                if (depFilterKey)
+                  if (selected) setTermsUrl(selectedDepartements.filter((e) => e !== depFilterKey).join(SEPARATOR));
+                  else setTermsUrl(selectedDepartements.concat(depFilterKey).join(SEPARATOR));
+              }}
+              d={p.d}
+              key={p.insee_dep}
+              id={p.insee_dep}
+              stroke="black"
+              fill={
+                termsUrl === "" || selected
+                  ? colorScale((dataByINSEEDep[p.insee_dep]?.doc_count || 0) / maxCount).hex()
+                  : "lightgrey"
+              }
+            />
+          );
+        })}
+      </svg>
+    </div>
   );
 };
