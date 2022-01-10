b03741c2cc46490b6e0c4d25e49ee97f0b412f76
Paul Girard <paul@ouestware.com>
2022-01-10 17:16:15 +0100

    [frontend] cartography in dashboard with fake data #32

--- frontend/src/pages/explore/ResultDashboard.tsx
+++ frontend/src/pages/explore/ResultDashboard.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { Cartography } from "../../components/viz/Cartography";
 import { ProfessionDeFoi, ESSearchQueryContext } from "../../types";
 
 interface ResultDashboardProps {
@@ -10,5 +11,34 @@ interface ResultDashboardProps {
 }
 export const ResultDashboard: FC<ResultDashboardProps> = ({ context, result }) => {
   console.log(context, result);
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <Cartography
+          height={700}
+          width={700}
+          data={[
+            {
+              "departement-insee": "44 - Loire-Atlantique",
+              doc_count: 4000,
+              "departement-nom": "Loire Atlantique",
+              departement: "44",
+            },
+            {
+              "departement-insee": "60 - Oise",
+              doc_count: 2000,
+              "departement-nom": "Oise",
+              departement: "60",
+            },
+            {
+              "departement-insee": "56 - Morbihan",
+              doc_count: 1000,
+              "departement-nom": "Morbihan",
+              departement: "56",
+            },
+          ]}
+        />
+      </div>
+    </div>
+  );
 };
