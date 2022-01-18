935a74f7614e8d4788ae2e452d06bcc26aaa30f1
Paul Girard <paul@ouestware.com>
2022-01-18 18:53:36 +0100

    [frontend] top values tyle

--- frontend/src/components/viz/TopValues.tsx
+++ frontend/src/components/viz/TopValues.tsx
@@ -3,33 +3,32 @@ import { FC } from "react";
 
 import { TopValuesDataType } from "../../types/viz";
 
-export const TopValues: FC<{ data: TopValuesDataType }> = ({ data }) => {
+export const TopValues: FC<{ title: string; data: TopValuesDataType }> = ({ title, data }) => {
   const maxValue = max(data.tops.map((d) => d.count)) || 0;
 
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <h2>Top {data.field}</h2>
-      </div>
+    <div className="w-100">
+      <h2>{title}</h2>
+      <div className="mb-3">Classement des {data.tops.length} occurences les plus grandes</div>
 
       <div className="w-100 mt-2 barchart">
         {data.tops.map((value, i) => {
           const widthPercentage = (value.count / maxValue) * 100;
+          const id = `${data.field}-${value.key}`;
           return (
             <>
-              <div key={`bar${i}`} className="d-flex justify-content-end bar-container">
-                {widthPercentage <= 10 && <span style={{ marginRight: "5px" }}>{value.count}</span>}
-                <div className="position-relative bar" style={{ height: "100%", width: `${widthPercentage}%` }}>
-                  {widthPercentage > 10 && (
-                    <span className="position-absolute" style={{ left: 5 }}>
-                      {value.count}
-                    </span>
-                  )}
+              <div id={id} key={`${id}-bar`} className="d-flex justify-content-end align-items-center bar-container">
+                {widthPercentage <= 90 && <span style={{ marginRight: "5px" }}>{value.count}</span>}
+                <div
+                  className="position-relative bar d-flex justify-content-start align-items-center"
+                  style={{ height: "100%", width: `${widthPercentage}%` }}
+                >
+                  {widthPercentage > 90 && <span style={{ marginLeft: "5px" }}>{value.count}</span>}
                 </div>
               </div>
-              <div key={`label${i}`} className="pl-2 label">
+              <label htmlFor={id} key={`${id}-label`} className="pl-2 label">
                 {value.key}
-              </div>
+              </label>
             </>
           );
         })}
